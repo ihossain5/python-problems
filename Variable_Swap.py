a7a7a665bc111e29d314d646d1e7029920ba8fe1
ihossain5 <ismail.hossain5169@gmail.com>
2024-08-01 22:55:13 +0600

    variable swap

---
+++ Variable_Swap.py
@@ -0,0 +1,19 @@
+name = 'ismail'
+age = 29
+
+name, age = age, name
+
+print(name, age)
+
+
+
+a = 11
+b = 7
+
+a = a + b # a = 18, b = 7
+print(a, b)
+b = a - b # a = 18, b = 11
+print(a, b)
+a = a - b # a = 7,  b = 11
+
+print(a, b)
