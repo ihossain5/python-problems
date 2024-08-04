3376916a49e915b2f66f51eb39dba4108f694c9b
ihossain5 <ismail.hossain5169@gmail.com>
2024-08-04 22:06:53 +0600

    sum added

---
+++ sum.py
@@ -0,0 +1,8 @@
+def sum(num1: float, num2: float) -> float:
+        return num1 + num2
+
+a = float(input('Enter the first number: '))
+b = float(input('Enter the second number: '))
+
+print("The sum is:", sum(a, b))
+    
