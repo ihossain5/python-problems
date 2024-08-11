66cb4fa352d40a1f9dd9cb922a3913505fe7bcf6
ihossain5 <ismail.hossain5169@gmail.com>
2024-08-11 22:32:17 +0600

    even or odd using ternery operator

--- evenOrOdd.py
+++ evenOrOdd.py
@@ -1,7 +1,10 @@
 def evenOrOdd(num: int):
-    if(num % 2 == 0):
-        return str(num) +' is  an even number'
-    return str(num) +' is an odd number'
+    # if(num % 2 == 0):
+    #     return str(num) +' is  an even number'
+    # return str(num) +' is an odd number'
+
+    #ternery operator
+    return str(num) +' is  an even number' if num % 2 == 0 else str(num) +' is an odd number'
 
 number = int(input('Enter the number:'))
 
